e75fcf689c06db14b227b5bcc5c31d4d3b7ea48d
tobiasg <tobiasg@users.noreply.github.com>
2025-06-25 10:31:26 +0200

    Create project

---
+++ LLTU2025_6_StorageApi/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace LLTU2025_6_StorageApi;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public string Category { get; set; }
+    public string Shelf { get; set; }
+    public int Count { get; set; }
+    public string Description { get; set; }
+}
