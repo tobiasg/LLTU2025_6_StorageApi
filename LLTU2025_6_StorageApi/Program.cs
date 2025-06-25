5d2b02ddf0033d5d6e062493ce716c35e3e34328
tobiasg <tobiasg@users.noreply.github.com>
2025-06-25 16:31:16 +0200

    Add swagger

--- LLTU2025_6_StorageApi/Program.cs
+++ LLTU2025_6_StorageApi/Program.cs
@@ -3,21 +3,20 @@ using Microsoft.Extensions.DependencyInjection;
 using LLTU2025_6_StorageApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ApplicationContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext") ?? throw new InvalidOperationException("Connection string 'ApplicationContext' not found.")));
-
-// Add services to the container.
-
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext") ?? throw new InvalidOperationException("Connection string 'ApplicationContext' not found.")));
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
